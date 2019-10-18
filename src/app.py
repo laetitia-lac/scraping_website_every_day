d2345d9dbe2031ca883431a7a25dd14cd04390d6
Laetitia <lae.lachat@gmail.com>
2019-10-18 18:08:18 +0200

    modify the trigger reason to send an email

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ from utils.logger import logger
 if __name__ == '__main__':
     content_list_projects = get_content_list_projects()
 
-    if content_list_projects != "Il n'y a pas de projet à traiter pour le momentF.":
+    if content_list_projects != "Il n'y a pas de projet à traiter pour le moment.":
         content_email = f'List of projects is not empty, first project: {content_list_projects}'
         logger.debug(content_email)
         send_email(content_email)
